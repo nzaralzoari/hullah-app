04007da6b2e6cdd9d33dd77f38f584362cc80f11
nzaralzoari <nzaralzoari@gmail.com>
2026-08-27 16:08:27 +0300

    Update import path for HullahDualInterfaceApp

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HullahDualInterfaceApp from "./components/HullahDualInterfaceApp.jsx";
+import HullahDualInterfaceApp from "./HullahDualInterfaceApp.jsx";
 
 export default function App() {
   return <HullahDualInterfaceApp />;
